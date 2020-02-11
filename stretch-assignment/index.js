e774cca2b68e9878cdb579e9fe4abf595a9907bc
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-02-11 11:52:47 -0800

    made click event for boxes

---
+++ stretch-assignment/index.js
@@ -0,0 +1,17 @@
+const blocks = document.querySelectorAll('.block');
+
+const redBlock = document.querySelector('.block--red');
+
+const blueBlock = document.querySelector('.block--blue');
+
+const greenBlock = document.querySelector('.block--green');
+
+const pinkBlock = document.querySelector('.block--pink');
+
+const grayBlock = document.querySelector('.block--gray');
+
+blocks.forEach(ele => {
+    ele.addEventListener('click', (event) => {
+        ele.parentNode.prepend(ele);
+    })
+})
